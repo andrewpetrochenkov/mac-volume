6fb430656adf4b6bdca1ea86dbef5d00c52903d9
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:09:04 +0300

    2020:12:04 00:09

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-volume',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/.mute.applescript','scripts/.unmute.applescript','scripts/.volume.applescript','scripts/mute','scripts/unmute','scripts/volume']
